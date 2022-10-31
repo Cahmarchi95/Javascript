d03114a73f4a929314fca37b050d902eb97ba363
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2022-10-31 12:14:57 -0300

    counter project

---
+++ 02-Objects/OPP - Projects/16-counter/starter/app-proto.js
@@ -0,0 +1,49 @@
+//função para checar se o elemento foi selecionado corretamente
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  } else {
+    throw new Error(
+      `Please check "${selection} selector, no such element exists"`
+    );
+  }
+}
+
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  console.log(this.resetBtn);
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+
+  this.increaseBtn.addEventListener ("click",this.increase.bind(this));
+  this.decreaseBtn.addEventListener ("click",this.decrease.bind(this));
+  this.resetBtn.addEventListener ("click",this.reset.bind(this));
+}
+
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+firstCounter.decrease();
+secondCounter.increase();
+secondCounter.reset();
