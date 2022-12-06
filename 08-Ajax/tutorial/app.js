b7e9d388392bfe5575ff90dce9005954dad15623
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2022-12-05 22:15:04 -0300

    api conceitos

---
+++ 08-Ajax/tutorial/app.js
@@ -0,0 +1,34 @@
+//JSON = Javascript Object Notation
+
+const xhr = new XMLHttpRequest();
+console.log(xhr);
+const btn = document.querySelector(".btn");
+const url = "./api/people.json";
+btn.addEventListener("click", () => {
+  getData(url);
+});
+
+function getData(url) {
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      //JSON.parse = transforma a string em objeto
+      const displayData = data
+        .map((item) => {
+          return `<p>${item.name}</p>`;
+        })
+        .join("");
+      const element = document.createElement("div");
+      element.innerHTML = displayData
+      document.body.appendChild(element)
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        sate: xhr.readyState,
+      });
+    }
+  };
+  xhr.send();
+}
