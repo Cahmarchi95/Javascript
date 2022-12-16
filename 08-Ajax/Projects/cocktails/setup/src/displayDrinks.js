391fc40162e684ec7fe6f77f04d9e5720e2bd950
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2022-12-15 21:48:21 -0300

    cocktail final

--- 08-Ajax/Projects/cocktails/setup/src/displayDrinks.js
+++ 08-Ajax/Projects/cocktails/setup/src/displayDrinks.js
@@ -1,17 +1,19 @@
 import get from "./getElement.js";
+import { hideLoading } from "./toggleLoading.js";
 
 const displayDrinks = ({ drinks }) => {
   const section = get(".section-center");
   const title = get(".title");
   if (!drinks) {
     // hide loading
+    hideLoading();
     title.textContent = "sorry, no drinks matched your search";
     section.innerHTML = null;
     return;
   }
   const newDrinks = drinks
     .map((drink) => {
-        const {idDrink:id, strDrink:name,strDrinkThumb:image} = drink
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
 
       return `<a href="drink.html">
           <article class="cocktail" data-id="${id}">
@@ -22,9 +24,10 @@ const displayDrinks = ({ drinks }) => {
     })
     .join("");
   //hide loading
+  hideLoading();
   title.textContent = "";
-  section.innerHTML = newDrinks
-  return section
+  section.innerHTML = newDrinks;
+  return section;
 };
 
 export default displayDrinks;
