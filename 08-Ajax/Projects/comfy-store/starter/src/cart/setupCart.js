e66bce18850e086eec3e797c67ea3e9bbb27baa8
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-11 22:55:46 -0300

    comfy project final

--- 08-Ajax/Projects/comfy-store/starter/src/cart/setupCart.js
+++ 08-Ajax/Projects/comfy-store/starter/src/cart/setupCart.js
@@ -28,6 +28,10 @@ export const addToCart = (id) => {
     addToCartDOM(product);
   } else {
     // update values
+    const amount = increaseAmount(id);
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
   }
   // add one to the item count
   displayCartItemCount();
@@ -53,14 +57,76 @@ function displayCartTotal() {
   cartTotalDOM.textContent = `Total : ${formatPrice(total)} `;
 }
 
-function setupCartFunctionality(){
-
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id);
 }
 
-function displayCartItemsDOM(){
-  cart.forEach((cartItem)=>{
-    addToCartDOM(cartItem)
-  })
+function increaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+
+function decreaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener("click", function (e) {
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
+
+    //remove
+    if (element.classList.contains("cart-item-remove-btn")) {
+      removeItem(id);
+      parent.parentElement.remove();
+    }
+
+    //increase
+
+    if (parent.classList.contains("cart-item-increase-btn")) {
+      const newAmount = increaseAmount(parentID);
+      parent.nextElementSibling.textContent = newAmount;
+    }
+
+    //decrease
+
+    if (parent.classList.contains("cart-item-decrease-btn")) {
+      const newAmount = decreaseAmount(parentID);
+      if (newAmount === 0) {
+        removeItem(parentID);
+        parent.parentElement.parentElement.remove();
+      } else {
+        parent.previousElementSibling.textContent = newAmount;
+      }
+    }
+
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem("cart", cart);
+  });
+}
+
+function displayCartItemsDOM() {
+  cart.forEach((cartItem) => {
+    addToCartDOM(cartItem);
+  });
 }
 
 const init = () => {
@@ -69,7 +135,7 @@ const init = () => {
   //display total
   displayCartTotal();
   // add all cart items to the DOM
-  displayCartItemsDOM()
+  displayCartItemsDOM();
   // setup cart functionality
   setupCartFunctionality();
 };
